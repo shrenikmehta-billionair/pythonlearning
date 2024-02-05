b5c1a110bcfca89bfaff8362e9255d79641461f1
shrenik27-tech <136146019+shrenik27-tech@users.noreply.github.com>
2024-02-05 22:34:13 +0530

    02-Feb task

---
+++ PythonLearning/lab_001.py
@@ -0,0 +1,9 @@
+name = input("Enter your name:")
+roll_no = input("Enter your roll no:")
+age = input("Enter your age:")
+phone_number = input("Enter your phone number:")
+
+print("Your Name is : ",name)
+print("Your roll_no is : ",roll_no)
+print("Your age is :",age)
+print("Your phone number  is : ",phone_number)
---
+++ PythonLearning/lab_002.py
@@ -0,0 +1,7 @@
+print("Please enter the number for Which table needs to generated:")
+table = input("please enter the number:")
+table1 = int(table)
+for i in range(1, 11):
+    print(table, 'x',i, '=', table1*i)
+
+
