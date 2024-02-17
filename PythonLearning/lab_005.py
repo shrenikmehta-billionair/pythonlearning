cbd686de1dee935af7e34a3e8372a4363e518bf5
shrenik27-tech <136146019+shrenik27-tech@users.noreply.github.com>
2024-02-17 19:25:00 +0530

    lab_011 and Lab_012 version 1

--- PythonLearning/lab_005.py
+++ PythonLearning/lab_005.py
@@ -9,12 +9,12 @@ ANS 2 ** is kind of raise to operator i.e. 3**2 means 3*3 = 9
 
 """
 # Generating area for a circle with radius as a input from user
-
+import math
 pie_value = 3.14
 radius = float(input("Enter the radius for a circle : "))
 
 Area_of_a_circle = (pie_value * radius * radius)
-
+#area = 3.14 * pow(radius,2)
 print("The area of circle is ", Area_of_a_circle)
 
-
+# area = 3.14*pow(radius,2)
